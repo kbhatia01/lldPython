7ef2c461ddafc09894714ae97fda8b4573eaa54e
kbhatia01 <karanbhtiacs@gmail.com>
2024-07-17 22:19:26 +0530

    lambda expression

--- lamda/main.py
+++ lamda/main.py
@@ -1,3 +1,4 @@
+from functools import reduce
 mul = lambda x,y : x*y
 
 print(mul(2,3))
@@ -16,5 +17,26 @@ print(square)
 even_filter = list(filter(lambda x: x%2==0, nums))
 
 print(even_filter)
+
+nums= [1,2,3,4,5]
+
+
+total = reduce(lambda x,y: x+y, nums)
+
+print(total)
+
+
+max_value = reduce(lambda x,y : max(x,y), nums)
+
+print(max_value)
+
+print(max(nums))
+
+words = ["hello", " ", "world"]
+
+conc = reduce(lambda x,y : x+y, list(filter(lambda word: word!=" ", words)))
+
+print(conc)
+
 
 
