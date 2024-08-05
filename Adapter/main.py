65daa9c5cc79d9f8c976db20425ceab5d4f878e8
kbhatia01 <karanbhtiacs@gmail.com>
2024-08-05 22:31:38 +0530

    rename

--- Adapter/main.py
+++ Adapter/main.py
@@ -1,6 +1,6 @@
 from Adapter.Adapters.IciciBankAdapter import IciciBankAdapter
 from Adapter.Adapters.yesBankAdapter import YesBankAdapter
-from Adapter.Phonepe import Payment
+from Adapter.Payment import Payment
 
 if __name__ == '__main__':
     b = YesBankAdapter()
